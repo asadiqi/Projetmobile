b6403cc1a5d238e583ba029309fb1182f1b76efb
asadiqi <asadiqi@helb-prigogine.be>
2025-02-13 23:22:09 +0100

    login

--- app/src/main/java/com/example/startxplanify/MainActivity.java
+++ app/src/main/java/com/example/startxplanify/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.startxplanify;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -16,7 +19,6 @@ import androidx.lifecycle.ViewModelProvider;
 
 public class MainActivity extends AppCompatActivity {
 
-    private  MyViewModel myViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +36,18 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main_land);
 
-        myViewModel = new ViewModelProvider(this).get(MyViewModel.class);
-        myViewModel.getMyData().observe(this,data -> {
-            TextView textView = findViewById(R.id.textView);
-            textView.setText(data);
+
+        Button getStartButton = findViewById(R.id.button);
+
+        getStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+            }
         });
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
